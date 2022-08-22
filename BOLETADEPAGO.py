3237e0f6b9e35cb1445939170027d1b5f72110e1
priscigu <110804906+priscigu@users.noreply.github.com>
2022-08-21 22:42:20 -0400

    Add files via upload

---
+++ BOLETADEPAGO.py
@@ -0,0 +1,12 @@
+nombre=int(input("Ingrese su nombre"))
+apellido=int(input("Ingrese su apellido"))
+carnetdeindentidad=int(input("Ingrese su carnet de identidad"))
+gestioningreso=int(input("Ingrese la gestion de ingreso"))
+cantidadhora=int(input("Ingrese la cantidad de horas trabajadas"))
+preciohorasaldo=int(input("Ingrese el precio por hotra"))
+sueldomes=int(input("Ingrese su sueldo del mes'))
+horasextras=int(input("Ingrese las horas extras trabajadas"))
+
+
+            
+                
